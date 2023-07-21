68e98c85ebe87833afa1bd337f289c58ae4df394
Rahul <rjain17609@gmail.com>
2023-07-20 21:07:38 -0700

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,12 @@
+package letslearngit;
+
+public class multiply {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("sum of two number" + (a + b));
+    }
+
+}
